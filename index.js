9bc1d53f7482124b0e490958a2cea901fe0f1385
dialloflatno <brahimdiallo23@gmail.com>
2021-10-04 21:02:38 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Write your solution here!
+const cats  = ['Milo', 'Otis', 'Garfield'];
+
+
+
+    function destructivelyAppendCat(name){
+        console.log("Name:",name);
+        cats.push('Ralph');
+        
+    }
+
+    function destructivelyPrependCat(name){
+        console.log("Name:", name);
+        cats.unshift('Bob');
+
+
+    }
+
+    function destructivelyRemoveFirstCat(name){
+         console.log("Name:",name);
+         cats.shift('Bob');
+
+
+    }
+
+    function destructivelyRemoveLastCat(name){
+        console.log("Name:",name);
+        cats.pop('Ralph');
+
+    }
+
+    function appendCat(name){
+        return [...cats,(name)];
+    } 
+    function prependCat(name){
+        return [(name),...cats];
+    }
+    function removeLastCat(name){
+        return cats.slice(0,length -1);
+    }
+
+    function removeFirstCat(name){
+        return cats.slice(1,cats.slicelength);
+    }
